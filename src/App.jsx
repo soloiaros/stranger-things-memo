68601954be764a3172376600841d58be1ceed45a
soloiaros <soloiaros@gmail.com>
2026-05-06 20:56:23 +0300

    feat: basic components setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from 'react'
+import ScoreScreen from './components/ScoreScreen'
+import CardScreen from './components/CardScreen'
 import './App.css'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <ScoreScreen />
+      <CardScreen />
+    </>
+  )
 }
 
 export default App
