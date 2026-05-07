eb8e7f5d0e2ffa2ff574536ea7f934cdda3d9235
soloiaros <soloiaros@gmail.com>
2026-05-07 13:42:43 +0300

    feat: make dialog box draggable

--- src/components/DialogBox.jsx
+++ src/components/DialogBox.jsx
@@ -1,8 +1,48 @@
+import { useRef, useEffect } from 'react'
 import CloseIcon from '../assets/close-btn.svg'
 
+function makeDraggable(dialogEl) {
+    let isDragging = false;
+    let initialX, initialY;
+
+    let [currentOffsetX, currentOffsetY] = [0, 0];
+
+    const handle = dialogEl.querySelector('.upper-bar');
+
+    function moveDialog(event) {
+        dialogEl.style.transform = `translate3d(${currentOffsetX + event.clientX - initialX}px, ${currentOffsetY + event.clientY - initialY}px, 0px)`;
+    } 
+    
+    handle.addEventListener('mousedown', (event) => {
+        initialX = event.clientX;
+        initialY = event.clientY;
+        isDragging = true;
+
+        document.addEventListener('mousemove', moveDialog);
+    });
+
+    document.addEventListener('mouseup', (event) => {
+        if (!isDragging) return;
+        currentOffsetX += event.clientX - initialX;
+        currentOffsetY += event.clientY - initialY;
+        isDragging = false;
+        document.removeEventListener('mousemove', moveDialog);
+    })
+}
+
 export default function DialogBox () {
+
+    const dialogRef = useRef(null);
+
+    useEffect(() => {
+        if (dialogRef.current) {
+            makeDraggable(dialogRef.current);
+        }
+    }, []);
+
+
     return (
-        <dialog id="attrib-dialog">
+        <dialog id="attrib-dialog" ref={dialogRef}>
             <div className="upper-bar">
                 <p>Attributions</p>
                 <form method="dialog"><button><img src={CloseIcon} alt="Close dialog" /></button></form>
