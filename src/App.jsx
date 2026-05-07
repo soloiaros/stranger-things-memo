4e62944822ccacb45d5cf04931378141a012cfb2
soloiaros <soloiaros@gmail.com>
2026-05-07 13:42:43 +0300

    style: add dividers and better card styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import NavigationBar from './components/NavigationBar'
 import ScoreScreen from './components/ScoreScreen'
 import CardScreen from './components/CardScreen'
 import './App.css'
@@ -6,22 +7,34 @@ import './App.css'
 function App() {
   const [bestScore, setBestScore] = useState(0);
   const [currScore, setCurrScore] = useState(0);
-  const [prevCards, setPrevCards] = useState([])
+  const [prevCards, setPrevCards] = useState([]);
+  const [gameMode, setGameMode] = useState('characters');
 
-  function handleCardSelection(charId) {
-    if (prevCards.includes(charId)) {
+  function handleCardSelection(cardUuid) {
+    if (prevCards.includes(cardUuid)) {
       setBestScore(currScore);
       setCurrScore(0);
     } else {
-      setPrevCards([...prevCards, charId]);
-      currScore += 1;
+      setPrevCards([...prevCards, cardUuid]);
+      setCurrScore(currScore + 1);
     }
   }
-;
+
+  function handleModeSwitch(event) {
+    if (event.target.getAttribute('data-mode') !== gameMode) {
+      setGameMode(event.target.getAttribute('data-mode'));
+      setCurrScore(0);
+      setBestScore(0);
+    }
+  }
+
   return (
     <>
       <div className="overlay"></div>
+      <NavigationBar onClick={handleModeSwitch} />
+      <div className="section-divider"></div>
       <ScoreScreen bestScore={bestScore} currScore={currScore} />
+      <div className="section-divider"></div>
       <CardScreen onClick={handleCardSelection} />
     </>
   )
