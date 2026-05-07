a5367b7a94d1a6ad808089f0b7da472ebea9ca02
soloiaros <soloiaros@gmail.com>
2026-05-07 13:42:43 +0300

    feat: add functional links section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
 import HPDLogo from '../assets/hawkpd.svg'
+import GHIcon from '../assets/gh.png'
+import CreditsIcon from '../assets/credits.png'
 
 export default function Header () {
     return (
         <header>
-            <img src={HPDLogo} alt="HPD Logo" />
-            <h1>Hawkins PD</h1>
-            <h2>Monitoring System</h2>
-            <h3>GCS v 1.2.9</h3>
-            <h3>© 1983 Global Control Systems Inc. </h3>
-            <h3> -Server 851- </h3>
-            <h3>------------------</h3>
+            <div className="decorative">
+                <img src={HPDLogo} alt="HPD Logo" />
+                <h1>Hawkins PD</h1>
+                <h2>Monitoring System</h2>
+                <h3>GCS v 1.2.9</h3>
+                <h3>© 1983 Global Control Systems Inc. </h3>
+                <h3> -Server 851- </h3>
+                <h3>------------------</h3>
+            </div>
+            <div className="functional-links">
+                <a href="https://github.com/soloiaros/stranger-things-memo" target='_blank'><img src={GHIcon} alt="GitHub repositoty of the project" /></a>
+                <a href="#"><img src={CreditsIcon} alt="View credentials" target='_blank' /></a>
+            </div>
         </header>
     )
 }
